556be090f6f992ca3513e6c61f9354e384b7acd2
Zack P <zack@zacklearns.com>
2017-08-18 19:42:13 -0600

    Create ___init__.py

---
+++ twig/___init__.py
@@ -0,0 +1,30 @@
+from random import *
+
+data = []
+
+class Chain:
+  def __init__():
+    print("Made new Markov Chain.")
+    
+  def AddWordToChain(w):
+    data.append(w)
+    
+  def AddWordsToChain(w):
+    data.extend(w)
+    
+  def GetChain(join):
+    chain = random.sample(data, 2)
+    return join.join(*chain)
+    
+class Waiter:
+  def __init__(period):
+    nexttime = time.time() + period
+    for i in count():
+        now = time.time()
+        tosleep = nexttime - now
+        if tosleep > 0:
+            time.sleep(tosleep)
+            nexttime += period
+        else:
+            nexttime = now + period
+        yield i, nexttime
